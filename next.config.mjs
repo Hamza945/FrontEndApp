f343fe7c4f4fe184e78bf3483d3f2fdaf7f8d090
Hamza Jamil <hamzaj@croyten.com>
2024-03-16 01:24:25 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Specify the base path for GitHub Pages
-    basePath: '/FrontEndApp',
-    // Configure export path
-    trailingSlash: true, // If your pages have trailing slashes
-    generateStaticParams: async () => {
-      // Return the list of paths to export
-      return {
-        '/': { page: '/' },
-        // Add other pages if necessary
-      };
-    },
-  };
-  
-  export default nextConfig;
-  
+    output: 'export',
+};
+
+export default nextConfig;
