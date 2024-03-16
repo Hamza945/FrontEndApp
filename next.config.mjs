177146a573f382e615117a1bdaa8984fa4a224bc
Hamza Jamil <hamzaj@croyten.com>
2024-03-16 12:31:29 +0500

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 // next.config.js
-const nextConfig = {
+module.exports = {
     // Specify the base path for GitHub Pages
     basePath: '/FrontEndApp',
     // Configure export behavior
-    output: 'export',
+    output: {
+      // Set the export target to 'export'
+      // This will generate a static version of your Next.js app
+      // that can be deployed to static hosting platforms like GitHub Pages
+      // For more options, you can refer to the Next.js documentation:
+      // https://nextjs.org/docs/api-reference/cli#output
+      export: {},
+    },
   };
-  
-  module.exports = nextConfig;
   
